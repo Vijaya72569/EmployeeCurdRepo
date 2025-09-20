196e13c617019950deba389b8f70831638663158
vijaya72569 <vijayaias2017@gmail.com>
2025-09-20 16:12:29 +0530

    first commit

---
+++ EmpCurdAzureRepo/Models/EmpDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmpCurdAzureRepo.Models
+{
+    public class EmpDbContext:DbContext
+    {
+        public EmpDbContext(DbContextOptions<EmpDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
